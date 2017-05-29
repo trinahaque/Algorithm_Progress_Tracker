71a7bdaded84426fe849674a34613ee552b6e8de
Trina Haque <trina_haque28@outlook.com>
2017-05-29 12:42:46 -0700

    made changes to urls.py

--- apps/algo_app/urls.py
+++ apps/algo_app/urls.py
@@ -29,17 +29,3 @@ urlpatterns = [
 
     url(r'^add_calendar$', views.add_calendar),
 ]
-
-# urlpatterns = [
-#     url(r'^$', views.index),
-#     url(r'^registration$', views.registration),
-#     url(r'^login$', views.login),
-#     url(r'^logout$', views.logout),
-#     url(r'^books$', views.books),
-#     url(r'^books/add$', views.add, name='add'),
-#     url(r'^add_book$', views.add_book),
-#     url(r'^books/(?P<bid>\d+)$', views.book, name='books'),
-#     url(r'^users/(?P<id>\d+)$', views.users),
-#     url(r'^add_review/(?P<id>\d+)/(?P<bid>\d+)$', views.review),
-#     url(r'^delete/(?P<rid>\d+)/(?P<bid>\d+)$', views.delete)
-# ]
